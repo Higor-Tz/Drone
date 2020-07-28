e2fb802ea7c704e976f5f5d34908a21a599697e9
Higor <higoth@usp.br>
2020-07-28 20:35:57 -0300

    Atualização do Canal de Comunicação

--- formacao/formacao/scripts/controle.py
+++ formacao/formacao/scripts/controle.py
@@ -2,12 +2,27 @@
 
 import sys
 import rospy
+import actionlib
+import actionlib_tutorials.msg
 from gazebo_msgs.msg import ModelState
+from std_msgs.msg import Int16
 from gazebo_msgs.srv import GetModelState
-from geometry_msgs.msg import Pose
+from geometry_msgs.msg import PoseStamped
 import tf
 
 modelname = ''
+
+
+#class SimpleActionClient(, ):
+#class MoveBaseAction(objetivo):
+#	def init(self, name):
+#		self._action_name = name
+#		self._as = actionlib.SimpleActionServer(self._action_name, actionlib_tutorials.msg.MoveBaseAction, execute_cb=self.execute_cb, auto_start = False)
+#		self._as.start()
+#	def execute_cb(self, goal):
+
+
+
 
 def quaternion2euler(orientation):
 	quaternion = (orientation.x,orientation.y,orientation.z,orientation.w)
@@ -18,30 +33,33 @@ def quaternion2euler(orientation):
 	return roll, pitch, yaw
 
 def callback(objetivo):
-	pub = rospy.Publisher('gazebo/set_model_state', ModelState, queue_size=10)
+	global indexold,flagBlock
+	print objetivo
+
+	pub = rospy.Publisher('gazebo/set_model_state', ModelState, queue_size=1)#10
 	rospy.wait_for_service('gazebo/get_model_state')
 	
 	GetState = rospy.ServiceProxy('gazebo/get_model_state', GetModelState)
 	selfState = GetState(modelname, 'world')
-	
+	#selfState = objetivo
 
 	comando = ModelState()
 	comando.pose.position.y = selfState.pose.position.y
 	comando.pose.position.z = selfState.pose.position.z
 	comando.pose.position.x = selfState.pose.position.x	
 	
-	comando.pose.orientation.x = selfState.pose.orientation.x
-	comando.pose.orientation.y = selfState.pose.orientation.y
-	comando.pose.orientation.z = selfState.pose.orientation.z
-	comando.pose.orientation.w = selfState.pose.orientation.w
+	#comando.pose.orientation.x = selfState.pose.orientation.x
+	#comando.pose.orientation.y = selfState.pose.orientation.y
+	#comando.pose.orientation.z = selfState.pose.orientation.z
+	#comando.pose.orientation.w = selfState.pose.orientation.w
 
 	self_roll, self_pitch, self_yaw = quaternion2euler(selfState.pose.orientation)
 	obj_roll, obj_pitch, obj_yaw = quaternion2euler(objetivo.orientation)
 
 	comando.twist.linear.x = 0.5*(objetivo.position.x - selfState.pose.position.x)
 	comando.twist.linear.y = 0.5*(objetivo.position.y - selfState.pose.position.y)
-	comando.twist.linear.z = 0.5*(objetivo.position.z - selfState.pose.position.z)
-
+	comando.twist.linear.z = 0.5*(objetivo.position.z - selfState.pose.position.z)	
+	
 	comando.twist.angular.x = 0.25*(obj_roll - self_roll)
 	comando.twist.angular.y = 0.25*(obj_pitch - self_pitch)
 	comando.twist.angular.z = 0.25*(obj_yaw - self_yaw)
@@ -50,29 +68,87 @@ def callback(objetivo):
 	
 	pub.publish(comando)
 
+	if(comando.twist.linear.x < 0.01 and comando.twist.linear.y < 0.01 and comando.twist.linear.z < 0.01):
+		indexold += 1
+
+	flagBlock = 0			
+
+
+# def controle(modelname):
+# 	rospy.init_node(modelname , anonymous=True)
+# 	objetivoMsgs = "objetivo_" + modelname
+# 	rospy.Subscriber('gazebo/waypoint', PoseStamped, callback)#objetivoMsgs, mudou antigo Pose
+	
+# 	#print rospy.search_param("modelName")
+# 	#modelname = rospy.get_param("modelName")
+	
+# 	rospy.spin()
+
+
+# if name == 'main':
+	
+# 	global indexold 
+# 	indexold = 0
+# 	print sys.argv
+# 	if len(sys.argv) > 1:
+# 		modelname = sys.argv[1]
+# 		print 'modelname:' + modelname
+# 	else:
+# 		print "Favor fornecer o nome de um model ativo"
+# 		sys.exit(1)
+# 	#try:
+# 	#	controle()
+# 	#except rospy.ROSInterruptException:
+# 	#	pass
+# 	controle(modelname)
+
+
+
+##### EXEMPLO
+
+
+def RunDrone():
+	print "Entrou em RunDrone"
+	global indexold,flagBlock
+	if(flagBlock == 0):
+		print "Passou flagBlock"
+		var = Int16()
+		var.data = indexold
+		pubind.publish(var)
+		flagBlock = 1
 
 
 def controle(modelname):
-	rospy.init_node(modelname , anonymous=True)
+	print "Entrou na funcao"
+	
 	objetivoMsgs = "objetivo_" + modelname
-	rospy.Subscriber(objetivoMsgs, Pose, callback)
-	
-	#print rospy.search_param("modelName")
-	#modelname = rospy.get_param("modelName")
-	
-	rospy.spin()
 
+	
+	rospy.init_node(modelname , anonymous=True)
+	rate = rospy.Rate(1) # 10hz
+
+	while not rospy.is_shutdown():
+		RunDrone()
+		rate.sleep()
 
 if __name__ == '__main__':
+
+	global indexold,flagBlock
+	indexold = 0
+	flagBlock = 0
 	print sys.argv
 	if len(sys.argv) > 1:
 		modelname = sys.argv[1]
 		print 'modelname:' + modelname
 	else:
 		print "Favor fornecer o nome de um model ativo"
-		sys.exit(1)
-	#try:
-	#	controle()
-	#except rospy.ROSInterruptException:
-	#	pass
-	controle(modelname)
+		sys.exit(1)	
+
+	pubind = rospy.Publisher('gazebo/link', Int16, queue_size=1)
+	rospy.Subscriber('gazebo/waypoint', PoseStamped, callback)#objetivoMsgs, mudou antigo Pose	
+
+	try:
+		print "entrou"
+		controle(modelname)
+	except rospy.ROSInterruptException:
+		pass
--- formacao/formacao/scripts/roteiro.py
+++ formacao/formacao/scripts/roteiro.py
@@ -3,45 +3,99 @@
 import rospy
 import actionlib
 import time
+import actionlib_tutorials.msg
+from std_msgs.msg import Int16
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-from geometry_msgs.msg import PoseWithCovarianceStamped, Quaternion
+from geometry_msgs.msg import PoseWithCovarianceStamped, Quaternion, PoseStamped
 from tf.transformations import quaternion_from_euler, euler_from_quaternion
 from math import radians
 
-LOCATIONS = [
-	[8.05, 0, 2, 0],
-	[8.05, 26, 2, 0],
-	[-1,95, 26, 2, 0],
-	[-1,95, 0, 2, 0],
-	[-1,95, 26, 2, 0],
-	[-11,95, 26, 2, 0],
-	[-11,95, 0, 2, 0],
-	[-11,95, 0, 0.81, 0]
-]
 
-def setupGoal(position):
-        x, y, z, yaw = position
-        goal = MoveBaseGoal()
-        goal.target_pose.header.frame_id = 'map'
-        goal.target_pose.pose.position.x = x
-        goal.target_pose.pose.position.y = y
-        goal.target_pose.pose.position.z = z
-        angle = radians(yaw)  # angles are expressed in radians
-        quaternion = quaternion_from_euler(0.0, 0.0, 0.0, angle)  # roll, pitch, yaw
-        goal.target_pose.pose.orientation = Quaternion(*quaternion.tolist())
+
+#rospy.init_node('roteiro' , anonymous=True)
+
+# def setupGoal(position):
+#         x, y, z, yaw = position #linha do Locations
+#         goal = MoveBaseGoal()
+#         goal.target_pose.header.frame_id = 'map'
+#         goal.target_pose.pose.position.x = x
+#         goal.target_pose.pose.position.y = y
+#         goal.target_pose.pose.position.z = z
+#         angle = radians(yaw)  # angles are expressed in radians
+#         quaternion = quaternion_from_euler(0, 0, angle)  # roll, pitch, yaw
+#         goal.target_pose.pose.orientation = Quaternion(*quaternion.tolist())
+#         return goal
+
+def setupGoal(index):
+        goal = PoseStamped()
+
+        LOCATIONS = [
+			[8.05, 0, 2, 0],
+			[8.05, 26, 2, 0],
+			[-1.95, 26, 2, 0],
+			[-1.95, 0, 2, 0],
+			[-1.95, 26, 2, 0],
+			[-11.95, 26, 2, 0],
+			[-11.95, 0, 2, 0],
+			[-11.95, 0, 0.81, 0]
+		]
+		
+#		x, y, z, yaw = position #linha do Locations
+#        goal = MoveBaseGoal()
+        goal.header.frame_id = 'map'
+        goal.pose.position.x = LOCATIONS[index][0]
+        goal.pose.position.y = LOCATIONS[index][1]
+        goal.pose.position.z = LOCATIONS[index][2]
+        #angle = radians(yaw)  # angles are expressed in radians
+        #quaternion = quaternion_from_euler(0, 0, angle)  # roll, pitch, yaw
+        #goal.target_pose.pose.orientation = Quaternion(*quaternion.tolist())
         return goal
 
-client = actionlib.SimpleActionClient('objetivo_lider', MoveBaseAction)
+#client = actionlib.SimpleActionClient('objetivo_lider', actionlib_tutorials.msg.MoveBaseAction)
 
-for l in LOCATIONS:
-    goal = setupGoal(l)
+def comunicacao(index):
+	goal = setupGoal(index)
+	pub.publish(goal)
+	print "publicou"
+	
 
-    client.wait_for_server()
-    client.send_goal(goal)
-    client.wait_for_result()
-    nav_result = client.get_result()
-    if nav_result:
-        print('Continuando rota...')
-    else:
-        print('Erro na rota!')
-        break
+def callback(index): # tratamento da recepicao
+	global indexold
+	indexnew = int(index.data)
+	print "entrou em callback"
+	if(indexnew!=indexold):
+		indexold = indexnew
+		print "recebeu o novo index"
+		comunicacao(indexnew)
+		print "saiu da comunicacao"
+		print indexold
+
+
+
+#for l in LOCATIONS:
+#    goal = setupGoal(l)
+
+#    client.wait_for_server()
+#    client.send_goal(goal)
+#    client.wait_for_result()
+#    nav_result = client.get_result()
+#    if nav_result:
+#        print('Continuando rota...')
+#    else:
+#        print('Erro na rota!')
+#        break
+
+if __name__ == '__main__':
+#	print sys.argv
+	global indexold
+	indexold = -1
+	rospy.Subscriber('gazebo/link', Int16, callback)# apelido do topico "gazebo/link" , tipo Int16 (inscreve neste canal de comunicacao)
+	pub = rospy.Publisher('gazebo/waypoint', PoseStamped, queue_size=1)# publica no topico (canal de comunicacao) "gazebo/waypoint" 
+	rospy.init_node('roteiro' , anonymous=True)
+	print "chegou aki"
+	rospy.spin()
+	
+
+
+
+
